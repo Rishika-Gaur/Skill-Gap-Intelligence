3be6a0a497853f50ba5c1615cc728214742b8cc5
Rishika <gotguitarinninthclass@gmail.com>
2026-01-31 17:26:13 +0530

    feat: env added

--- backend/app/utils/config.py
+++ backend/app/utils/config.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
+    ENV: str = os.getenv("ENV")
     MONGO_URI: str | None = os.getenv("MONGO_URI")
-    ENV: str = os.getenv("ENV", "development")
+    HOST: str = os.getenv("HOST")
+    PORT: int = int(os.getenv("PORT"))
+    DB_NAME: str = os.getenv("DB_NAME")
 
 settings = Settings()
